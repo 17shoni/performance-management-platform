b3f59a1ea8d0258da61f3356f0bc18875f88230f
Victor Mutua <17victormutua@gmail.com>
2026-02-02 13:44:08 +0300

    Update API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/', r
+  baseURL: 'https://performance-management-platform-production.up.railway.app/admin/', 
 });
 
 api.interceptors.request.use((config) => {
