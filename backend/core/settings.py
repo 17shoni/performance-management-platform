2d0a74e2968f2dd21bc3d33a359c64b6c90575e5
Victor Mutua <17victormutua@gmail.com>
2026-02-03 13:36:01 +0300

    disable DEBUG for first superuser

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get("DEBUG") == "True"
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") == "True"
+# DEBUG = True
 
 
 
