ac36b46a9cf083a7ec39fb219201668b23c2a730
Victor Mutua <17victormutua@gmail.com>
2026-02-11 20:35:52 +0300

    Fix cors

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -54,9 +54,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -107,6 +107,7 @@ DATABASES = {
 #    }
 # }
 
+CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
     "https://performance-management-platform.vercel.app",
